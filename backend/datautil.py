f2e48496d3ff7fbe652db3f9ddb4cb0240b5ad35
mehakmehta21 <mehakeck@gmail.com>
2015-04-24 01:29:26 -0400

    insertionin mongodn and returning reqid to ui

--- backend/datautil.py
+++ backend/datautil.py
@@ -1,25 +1,59 @@
 from pymongo import MongoClient
-
+import json
+from ReqResObjects import *
 
 ip = 'localhost'
 port = 27017
 
-DB = 'secaffe'
-CONNECTTABLE = 'connect_scan'
-ISUPTABLE = 'is_up_scan'
-FINTABLE = 'tcp_fin_scan'
-SYNTABLE = 'tcp_syn_scan'
+# DB = 'secaffe'
+# CONNECTTABLE = 'connect_scan'
+# ISUPTABLE = 'is_up_scan'
+# FINTABLE = 'tcp_fin_scan'
+# SYNTABLE = 'tcp_syn_scan'
+
+def preprocess(jsondata):
+    data2 = {}
+    data1 = []
+    data = {}
+    #SYN FYN
+    numres= len(jsondata["report"])
+    print numres
+    for i in xrange(numres):
+        data2 = {}
+        print i
+        data2["reqId"] = jsondata["reqId"]
+        data2["scannedports"] = str(min(jsondata["ports"]))+"-"+str(max(jsondata["ports"]))
+        data2["ip"] = jsondata["report"][i][0]
+        data2["openports"] = ','.join(map(str,jsondata["report"][i][1]))
+        data2["IPs"] = ','.join(map(str,jsondata["IPs"][i][1]))
+        print data2
+        data1.append(data2)
+    print json.dumps(data1)
+    #openportstr = jsondata["report"][0][0]
+
 
 
 def setup():
     return MongoClient(ip, port)
 
-
-def insertdata(client, json_data):
+def insertdata(client, json_data, scanType):
     # client = MongoClient(ip,port)
-    db = client.DB
-    records = db.is_up_scan
-    print records.insert(json_data)
+    db = client.secaffe
+
+    if(scanType==CONNECT_SCAN):
+        records = db.connect_scan
+    elif(scanType==IS_UP_BULK):
+        records = db.is_up_scan
+    elif(scanType==TCP_FIN_SCAN):
+        records = db.tcp_fin_scan
+    elif(scanType==TCP_SYN_SCAN):
+        records = db.tcp_syn_scan
+
+    jsond= json.loads(json_data)
+    print type(jsond)
+    print jsond
+
+    print records.insert(jsond)
     print "End"
 
 
